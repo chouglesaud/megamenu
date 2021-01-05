f76aa6d1f9dda8a0676f56f68b1f6da341ec4287
saud <saudchougle.sc@gmail.com>
2021-01-05 13:23:33 +0530

    done

--- public/scripts/loadView.js
+++ public/scripts/loadView.js
@@ -1,42 +1,38 @@
 const container = '#app';
-const simpleForm = `
-          <form>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Email address</label>
-              <input
-                type="email"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                placeholder="Enter email"
-              />
-              <small id="emailHelp" class="form-text text-muted"
-                >We'll never share your email with anyone else.</small
-              >
-            </div>
-            <div class="form-group">
-              <label for="exampleInputPassword1">Password</label>
-              <input
-                type="password"
-                class="form-control"
-                id="exampleInputPassword1"
-                placeholder="Password"
-              />
-            </div>
-            <div class="form-check">
-              <input
-                type="checkbox"
-                class="form-check-input"
-                id="exampleCheck1"
-              />
-              <label class="form-check-label" for="exampleCheck1"
-                >Check me out</label
-              >
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>`;
 
-const allSubmenuLinks = document.querySelectorAll('.sub-menu a');
+const form = {
+  'company-details': `<p>company detail form</p>`,
+  'employee-details': `<p>employee detail form</p>`,
+  'supplier-details': `<p>supplier detail form</p>`,
+  'work-nature-master': `<p>work-nature-master form</p>`,
+  'attendance-process-master': `<p>attendance-process-master form</p>`,
+  'item-master': `<p>item-master form</p>`,
+  'category-master': `<p>category-master form</p>`,
+  'designation-master': `<p>designation-master form</p>`,
+};
+const allSubmenuLinks = document.querySelectorAll('.sub-menu a[data-report]');
 
-console.log(allSubmenuLinks);
-document.querySelector(container).innerHTML = simpleForm;
+allSubmenuLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    loadFormBy(e.target.getAttribute('data-report'));
+  });
+});
+
+function loadFormBy(name) {
+  document.querySelector(container).innerHTML = form[name];
+}
+fetchForm();
+
+function fetchForm() {
+  fetch(
+    'http://mbhmoffice.azurewebsites.net/simpl_frm_interface.aspx?aa=tx%2b0ioQ2cB1dPJNAio3plQ%3d%3d',
+    {
+      headers: {
+        'Access-Control-Allow-Origin': 'http://mbhmoffice.azurewebsites.net',
+      },
+    }
+  ).then((data) => {
+    console.log(data);
+  });
+}
