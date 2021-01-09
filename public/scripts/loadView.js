4f57d3a0418ad541b74b6ef61b186ee716a4e517
saud <saudchougle.sc@gmail.com>
2021-01-09 17:51:52 +0530

    change

--- public/scripts/loadView.js
+++ public/scripts/loadView.js
@@ -203,54 +203,12 @@ const form = {
   </div>
 </div>`,
   'shredder-summary-report': `
-<form class="form-row">
-<div class="col-lg-5 col-md-8 col-sm-10 form-section">
-  <div class="form-group">
-    <label for="material">Material</label>
-    <select class="custom-select form-control" id="material">
-      <option selected>Select Material</option>
-      <option value="AMPOOLS">AMPOOLS</option>
-      <option value="INJECTION">INJECTION</option>
-      <option value="MIX-COVER">MIX COVER</option>
-      <option value="SALAINE-CAP">SALAINE CAP</option>
-      <option value="WASTE">WASTE</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="total-batch-no">Total Batch no</label>
-    <input
-      type="text"
-      class="form-control"
-      id="total-batch-no"
-    />
-  </div>
-  <div class="form-group">
-    <label for="total-bag">Total Bag</label>
-    <input type="text" class="form-control" id="total-bag" />
-  </div>
-  <div class="form-group">
-    <label for="total-kg">Total KG</label>
-    <input type="text" class="form-control" id="total-kg" />
-  </div>
-  <div class="form-group">
-    <label for="quantity-per-hr-kg"
-      >Quantity Per HR. KG</label
-    >
-    <input
-      type="text"
-      class="form-control"
-      id="quantity-per-hr-kg"
-    />
-  </div>
-  <div class="form-group" style="justify-content: flex-end">
-    <button class="btn btn-primary btn-sm" id="addGrid">ADD</button>
-  </div>
-</div>
-<div class="col-lg-5 col-md-8 col-sm-10 form-section">
-  <div class="form-group" style="justify-content: flex-start">
-    <p><b>Common Element</b></p>
-  </div>
-  <div class="form-group">
+ 
+<form class="form-row ">
+
+<div class="col-lg-3 col-md-3 col-sm-12 form-section grid-container">
+  
+  <div class="form-group grid-form-group">
     <label for="date-of-birth">Date</label>
     <input
       type="date"
@@ -258,7 +216,7 @@ const form = {
       id="date-of-birth"
     />
   </div>
-  <div class="form-group">
+  <div class="form-group grid-form-group">
     <label for="start-reading">Start Reading</label>
     <input
       type="text"
@@ -266,7 +224,7 @@ const form = {
       id="start-reading"
     />
   </div>
-  <div class="form-group">
+  <div class="form-group grid-form-group">
     <label for="close-reading">Close Reading</label>
     <input
       type="text"
@@ -274,7 +232,7 @@ const form = {
       id="close-reading"
     />
   </div>
-  <div class="form-group">
+  <div class="form-group grid-form-group">
     <label for="electricity-consumption"
       >Electricity Consumption</label
     >
@@ -284,9 +242,117 @@ const form = {
       id="electricity-consumption"
     />
   </div>
-  <div class="form-group" style="justify-content: flex-end">
-    <button class="btn btn-primary btn-sm">UPDATE</button>
-  </div>
+  <div class="form-group grid-form-group">
+  <label for="start-time"
+    >Start Time</label
+  >
+  <input
+    type="time"
+    class="form-control"
+    id="start-time"
+  />
+</div>
+<div class="form-group grid-form-group">
+  <label for="start-time"
+    >Start Time</label
+  >
+  <input
+    type="time"
+    class="form-control"
+    id="start-time"
+  />
+</div>
+ 
+</div>
+<div class="col-lg-9 col-md-9 col-sm-12 form-section grid" style="padding: 0px 10px;">
+<div class="col-12"  style="display: flex;justify-content: flex-end;padding: 5px 20px;"><button data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo" class="btn btn-success btn-sm">Add New</button></div>
+   
+   <div
+   class="modal fade bd-example-modal-lg"
+   id="exampleModal"
+   tabindex="-1"
+   role="dialog"
+   aria-labelledby="exampleModalLabel"
+   aria-hidden="true"
+ >
+   <div class="modal-dialog modal-lg" role="document">
+     <div class="modal-content">
+       <div class="modal-header">
+         <h5 class="modal-title" id="exampleModalLabel">Add New</h5>
+         <button
+           type="button"
+           class="close"
+           data-dismiss="modal"
+           aria-label="Close"
+         >
+           <span aria-hidden="true">&times;</span>
+         </button>
+       </div>
+       <div class="modal-body">
+         <form class="form">
+           <div class="col-8 form-section">
+             <div class="form-group">
+               <label for="material">Material</label>
+               <select
+                 class="custom-select form-control"
+                 id="material"
+               >
+                 <option selected>Select Material</option>
+                 <option value="AMPOOLS">AMPOOLS</option>
+                 <option value="INJECTION">INJECTION</option>
+                 <option value="MIX-COVER">MIX COVER</option>
+                 <option value="SALAINE-CAP">SALAINE CAP</option>
+                 <option value="WASTE">WASTE</option>
+               </select>
+             </div>
+             <div class="form-group">
+               <label for="total-batch-no">Total Batch no</label>
+               <input
+                 type="text"
+                 class="form-control"
+                 id="total-batch-no"
+               />
+             </div>
+             <div class="form-group">
+               <label for="total-bag">Total Bag</label>
+               <input
+                 type="text"
+                 class="form-control"
+                 id="total-bag"
+               />
+             </div>
+             <div class="form-group">
+               <label for="total-kg">Total KG</label>
+               <input
+                 type="text"
+                 class="form-control"
+                 id="total-kg"
+               />
+             </div>
+             <div class="form-group">
+               <label for="quantity-per-hr-kg"
+                 >Quantity Per HR. KG</label
+               >
+               <input
+                 type="text"
+                 class="form-control"
+                 id="quantity-per-hr-kg"
+               />
+             </div>
+             <div
+               class="form-group"
+               style="justify-content: flex-end"
+             ></div>
+           </div>
+         </form>
+       </div>
+       <div class="modal-footer">
+         <button type="button" class="btn btn-primary btn-sm">
+           ADD
+         </button>
+       </div>
+   </div>
+ </div>
 </div>
 </form>`,
 };
@@ -301,10 +367,6 @@ document.addEventListener('gridLoaded', () => {
   const totalKg = document.querySelector('#total-kg');
   const quantityPerHrKg = document.querySelector('#quantity-per-hr-kg');
   addGridBtn = document.querySelector('#addGrid');
-  addGridBtn.addEventListener('click', (e) => {
-    // console.log(material, totalBatchNo, totalBag, totalKg, quantityPerHrKg);
-    alert('hi');
-  });
 });
 class Table {
   constructor(id, heads, rows) {
@@ -314,10 +376,12 @@ class Table {
   }
   render() {
     document.dispatchEvent(new Event('gridLoaded'));
-    return `<div class="col-12"><table class="table table-hover table-sm" id="${
+    const table = `<div class="col-12"><table class="table table-hover table-sm" id="${
       this.id
     }">
           <thead>
+            <th> </th>
+            <th> </th>
             ${this._addHeads()}
           </thead>
           <tbody id="tableBody">
@@ -325,13 +389,14 @@ class Table {
           </tbody>
         </table>
       </div>`;
+    document.querySelector('.grid').innerHTML += table;
   }
   _addHeads() {
     let tableHeads = '';
     this.heads.forEach((head) => {
       tableHeads += `<th scope="col">${head}</th>`;
     });
-    return `<tr>${tableHeads}</tr>`;
+    return `${tableHeads}`;
   }
   _addRows() {
     let tableRows = '';
@@ -362,11 +427,52 @@ const tableHeads = [
   'Quantity Per HR.kg',
 ];
 const tableRows = [
-  ['AMPOOLS', '12', '2', '120', '50'],
-  ['INJECTION', '13', '3', '121', '51'],
-  ['MIX COVER', '14', '4', '122', '52'],
-  ['SALAINE CAP', '15', '5', '123', '53'],
-  ['WASTE', '16', '5', '124', '54'],
+  [
+    '<i class="bi bi-pencil-square"></i>',
+    '<i class="bi bi-trash-fill"></i>',
+    'AMPOOLS',
+    '12',
+    '2',
+    '120',
+    '50',
+  ],
+  [
+    '<i class="bi bi-pencil-square"></i>',
+    '<i class="bi bi-trash-fill"></i>',
+    'INJECTION',
+    '13',
+    '3',
+    '121',
+    '51',
+  ],
+  [
+    '<i class="bi bi-pencil-square"></i>',
+    '<i class="bi bi-trash-fill"></i>',
+    'MIX COVER',
+    '14',
+    '4',
+    '122',
+    '52',
+  ],
+  [
+    '<i class="bi bi-pencil-square"></i>',
+    '<i class="bi bi-trash-fill"></i>',
+    'SALAINE CAP',
+    '15',
+    '5',
+    '123',
+    '53',
+  ],
+
+  [
+    '<i class="bi bi-pencil-square"></i>',
+    '<i class="bi bi-trash-fill"></i>',
+    'WASTE',
+    '16',
+    '5',
+    '124',
+    '54',
+  ],
 ];
 
 const table = new Table('table01', tableHeads, tableRows);
@@ -384,23 +490,27 @@ allSubmenuLinks.forEach((link) => {
 function loadFormBy(name, id, type) {
   const appId = '#app';
   const app = document.querySelector(appId);
-  app.innerHTML = renderTopBarWith(name);
-  app.innerHTML += form[id];
+
   if (type === 'grid') {
-    app.innerHTML += table.render();
+    app.innerHTML = renderTopBarWith(name, 'SAVE');
+    app.innerHTML += form[id];
+    table.render();
+  } else {
+    app.innerHTML = renderTopBarWith(name, 'ADD');
+    app.innerHTML += form[id];
   }
 }
-function renderTopBarWith(name) {
-  return `<div class="top-bar">
-  <div class="form-label-contaier">
+function renderTopBarWith(name, btnName) {
+  return `<div class="top-bar row">
+  <div class="form-label-container col-lg-9 col-md-9 col-sm-12">
     <h5>${name}</h5>
   </div>
-  <div class="action-container">
-    <div class="back-btn-container">
-      <button class="btn btm-md btn-light">BACK</button>
+  <div class="action-container col-lg-2 col-md-2 col-sm-12">
+    <div class="back-btn-container btn-container">
+      <button class="btn btn-sm">BACK</button>
     </div>
-    <div class="add-btn-container">
-      <button class="btn btn-md btn-primary">ADD</button>
+    <div class="add-btn-container btn-container">
+      <button class="btn btn-sm btn-primary">${btnName}</button>
     </div>
   </div>
 </div>`;
